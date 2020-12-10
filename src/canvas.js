7e12e85fc01e609acfd7c796632c9d7ccaee7079
dinnosaur <dinnoroni@gmail.com>
2020-12-10 21:31:32 +0000

    Blending warp and weft patterns

--- src/canvas.js
+++ src/canvas.js
@@ -5,19 +5,96 @@ const Canvas = props => {
   const canvasRef = useRef(null)
   
   const draw = (ctx) => {
-    const colours = ['#000000','#ff0000']
+    const coloursWeft = ['rgb(0,255,255)','rgb(255,0,255)']
+    const coloursWarp = ['rgb(255,0,255)','rgb(0,255,255)']
+    var i =  0
+    var x = 0
+
+    ctx.globalCompositeOperation = 'multiply';
+
     ctx.beginPath()
-    for(var i = 0; i < 300; i + 30) {
-        ctx.fillStyle = '#000000'
-        ctx.fillRect(i,0,i + 30,200);
-        ctx.fillStyle = '#ff0000'
-        ctx.fillRect(i + 30,0,i + 30,200);
-        
-    }
-    ctx.fill()
-    // ctx2.fill()
-    
-    }
+      // // for(var i = 0; i < colours.length; i ++) {
+      //   console.log(i)
+      //   var x =+ i  
+      //   ctx.fillStyle = colours[i]
+      //   ctx.fillRect(0 ,0 ,150,200 );
+      //   ctx.fillStyle = colours[i + 1]
+      //   ctx.fillRect(150,0,300/colours.length,200);
+      //   
+      // // }
+    //   for(i = 0; i <= 300; i+=30) {
+    //     console.log(i)
+    //       x += i
+    //     ctx.fillStyle = colours[0]
+
+    //     ctx.fillRect(i + x ,0 , 30,200 );
+     
+    //     ctx.fillStyle = colours[1]
+    //     ctx.fillRect(30 + i + x,0, 30,200);      
+    // }
+
+
+let canvasheight = 200;
+
+let canvaswidth = 300; //canvas width
+
+let patternwidth = 30; //single pattern width
+
+let patternheight = 30;
+
+let noofweftpatterns = canvasheight/(patternheight * coloursWeft.length);
+
+let noofpatterns = canvaswidth/(patternwidth * coloursWarp.length); //total number of patterns
+
+let xaxisstart = 0;
+
+let yaxisstart = 0;
+
+
+
+var createPatternRepeat = () => {
+
+  for(var c= 0; c < coloursWarp.length; c++) {
+      warpPattern(c)
+      weftPattern(c)
+  }
+
+ 
+};
+
+const warpPattern = (c) => {
+ 
+  ctx.fillStyle = coloursWarp[c];
+  
+  ctx.fillRect(xaxisstart ,0 , patternwidth,200 );
+  xaxisstart = xaxisstart + patternwidth;
+  
+}
+
+const weftPattern = (c) => {
+  
+  ctx.fillStyle = coloursWeft[c]
+  
+  ctx.fillRect(0, yaxisstart, 300, patternheight);
+  yaxisstart = yaxisstart + patternheight  
+}
+
+
+for( i=0; i< noofpatterns;i++)  {
+  createPatternRepeat()
+}
+
+  //   for(var c= 0; c < colours.length; c++) {
+      
+  //     ctx.fillStyle = colours[c]
+  //     ctx.fillRect(xi ,0 , warpwidth,200 );
+  //     xi = xi+warpwidth;
+
+
+  // }
+
+
+   }
 
   useEffect(() => {
     const canvas = canvasRef.current
@@ -29,7 +106,7 @@ const Canvas = props => {
         
   return (
     <div>
-        <canvas width="300" height="150" style={{border: `1px solid #d3d3d3`}} ref={canvasRef} {...props}/>
+        <canvas width="300" height="200" style={{border: `1px solid #d3d3d3`}} ref={canvasRef} {...props}/>
     </div>
   )
 }
